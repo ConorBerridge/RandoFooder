aaf1e4c416789de8530833f486555ec3b70848cb
ConorBerridge <conorberridge@gmail.com>
2017-12-23 12:35:44 +0000

    Starting on writing to file. Doesn't work yet because of an invalid file path I suspect.

--- app/src/main/java/co/conor/randofooder/FoodListEditor.java
+++ app/src/main/java/co/conor/randofooder/FoodListEditor.java
@@ -3,7 +3,6 @@ package co.conor.randofooder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,7 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,10 +25,13 @@ import java.util.Comparator;
 public class FoodListEditor extends AppCompatActivity {
 
 
+    static String selectedValue;
     ListView secondList;
     ArrayAdapter secondAdapter;
     ArrayList<String> allFood = new ArrayList<String>(MainActivity.getAllFoods());
-    static String selectedValue;
+    Context context = FoodListEditor.this;
+    File path = context.getExternalFilesDir(null);
+    File file = new File(path, "RandoFooderFoodList.txt");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +42,13 @@ public class FoodListEditor extends AppCompatActivity {
         this.popList();
 
 
-
-        secondList = (ListView)findViewById(R.id.editFoodList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2, android.R.id.text1, allFood);
+        secondList = (ListView) findViewById(R.id.editFoodList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text1, allFood);
 
         secondList.setAdapter(adapter);
 
         // ListView on item selected listener.
-        secondList.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        secondList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
@@ -57,9 +59,12 @@ public class FoodListEditor extends AppCompatActivity {
 
             }
         });
+
+        this.writeToFile();
+        this.readFile();
     }
 
-    public void popList(){
+    public void popList() {
         allFood = MainActivity.getAllFoods();
         Collections.sort(allFood, new Comparator<String>() {
             @Override
@@ -70,36 +75,75 @@ public class FoodListEditor extends AppCompatActivity {
 
         secondList = (ListView) findViewById(R.id.editFoodList);
 
-        secondAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, allFood);
+        secondAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, allFood);
 
         secondList.setAdapter(secondAdapter);
     }
 
 
-   public void backOnClick(View a){
+    public void backOnClick(View a) {
         Button button = (Button) a;
 
-       startActivity(new Intent(FoodListEditor.this , MainActivity.class));
+        startActivity(new Intent(context, MainActivity.class));
 
     }
 
-    public void deleteOnClick(View a){
+    public void deleteOnClick(View a) {
         Button button = (Button) a;
         MainActivity.getAllFoods().remove(selectedValue);
         this.popList();
 
-        Toast.makeText(FoodListEditor.this, selectedValue + " deleted", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, selectedValue + " deleted", Toast.LENGTH_SHORT).show();
     }
 
-    public void saveOnClick(View a){
+    public void saveOnClick(View a) {
         Button button = (Button) a;
-        TextView textView = (TextView)findViewById(R.id.SelText);
+        TextView textView = (TextView) findViewById(R.id.SelText);
         String temp = textView.getText().toString();
         MainActivity.addFood(temp);
         this.popList();
+        this.writeToFile();
     }
 
 
+    public void writeToFile() {
+        FileOutputStream stream = null;
+        try {
+            stream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            stream.write("TESTESTETSTETSTETSTESTSETETSETESTESTSET".getBytes());
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile() {
+        int length = (int) file.length();
+
+        byte[] bytes = new byte[length];
+
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            in.read(bytes);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String contents = new String(bytes);
+
+        Toast.makeText(context, contents, Toast.LENGTH_SHORT).show();
+
+    }
 
 
 }
